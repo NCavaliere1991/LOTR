420af8ea9e7b3fe20051a8ba745d14c89d9ea9fe
Nicolas Cavaliere <75913062+NCavaliere1991@users.noreply.github.com>
2021-03-02 19:22:14 -0500

    Add files via upload

---
+++ character_list.py
@@ -0,0 +1,5 @@
+characters_with_quotes = ["Aragorn II Elessar", "Gandalf", "Frodo Baggins", "Samwise Gamgee", "Legolas", "Gimli",
+                          "Peregrin Took", "Meriadoc Brandybuck", "Boromir", "Gollum", "Sauron", "Éowyn", "Éomer",
+                          "Saruman", "Treebeard", "Théoden", "Elrond", "Galadrial", "Faramir", "Bilbo Baggins", "Arwen",
+                          "Denethor I", "Gríma Wormtongue", "Witch-King of Angmar", "Gothmog (Lieutenant of Morgul)",
+                          "The King of the Dead", "Haldir (Haladin)"]
