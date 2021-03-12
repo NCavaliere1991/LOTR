1b6fed9d9c1449d76ef65f079c34f204fb7515c0
Nicolas Cavaliere <75913062+NCavaliere1991@users.noreply.github.com>
2021-03-12 17:07:45 -0500

    Update character_list.py

--- character_list.py
+++ character_list.py
@@ -3,3 +3,33 @@ characters_with_quotes = ["Aragorn II Elessar", "Gandalf", "Frodo Baggins", "Sam
                           "Saruman", "Treebeard", "Théoden", "Elrond", "Galadrial", "Faramir", "Bilbo Baggins", "Arwen",
                           "Denethor I", "Gríma Wormtongue", "Witch-King of Angmar", "Gothmog (Lieutenant of Morgul)",
                           "The King of the Dead", "Haldir (Haladin)"]
+
+pic_urls = {
+    "Aragorn II Elessar": "https://www.denofgeek.com/wp-content/uploads/2018/05/viggo-mortensen-aragorn-lord-of-the-rings-the-two-towers.jpg?fit=620%2C368",
+    "Gandalf": "https://static.wikia.nocookie.net/lotr/images/e/e7/Gandalf_the_Grey.jpg/revision/latest/scale-to-width-down/700?cb=20121110131754",
+    "Frodo Baggins": "https://static.wikia.nocookie.net/lotr/images/5/54/Untitledjk.png/revision/latest/scale-to-width-down/700?cb=20130313174543",
+    "Samwise Gamgee": "https://static.wikia.nocookie.net/lotr/images/2/20/Sam.jpg/revision/latest/scale-to-width-down/620?cb=20070623123241",
+    "Legolas": "https://static.wikia.nocookie.net/lotr/images/3/33/Legolas_-_in_Two_Towers.PNG/revision/latest/scale-to-width-down/699?cb=20120916035151",
+    "Gimli": "https://static.wikia.nocookie.net/lotr/images/e/ec/Gimli_-_FOTR.png/revision/latest/scale-to-width-down/700?cb=20121008105956",
+    "Peregrin Took": "https://static.wikia.nocookie.net/lotr/images/0/0a/Pippinprintscreen.jpg/revision/latest/scale-to-width-down/398?cb=20060310083048",
+    "Meriadoc Brandybuck": "https://static.wikia.nocookie.net/lotr/images/d/d8/Merry1.jpg/revision/latest/scale-to-width-down/700?cb=20080318214905",
+    "Boromir": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lord-of-the-rings-sean-bean-boromir-1584636601.jpg",
+    "Gollum": "https://static.wikia.nocookie.net/lotr/images/e/e1/Gollum_Render.png/revision/latest/scale-to-width-down/496?cb=20141218075509",
+    "Sauron": "https://static.wikia.nocookie.net/lotr/images/9/90/Sauron-2.jpg/revision/latest/scale-to-width-down/600?cb=20110508182634",
+    "Éowyn": "https://static.wikia.nocookie.net/lotr/images/3/30/Eowyn.jpg/revision/latest/scale-to-width-down/682?cb=20070624161202",
+    "Éomer": "https://static.wikia.nocookie.net/lotr/images/b/b9/Eomer_-_Close_up.PNG/revision/latest/scale-to-width-down/700?cb=20120922113500",
+    "Saruman": "https://static.wikia.nocookie.net/lotr/images/0/0c/Christopher_Lee_as_Saruman.jpg/revision/latest/scale-to-width-down/687?cb=20170127113833",
+    "Treebeard": "https://static.wikia.nocookie.net/lotr/images/2/23/TreebeardatIsengard.png/revision/latest/scale-to-width-down/699?cb=20210117035345",
+    "Théoden": "https://static.wikia.nocookie.net/lotr/images/1/13/King_Theoden_1.jpg/revision/latest/scale-to-width-down/700?cb=20150313135224",
+    "Elrond": "https://static.wikia.nocookie.net/lotr/images/9/9f/Elrond_of_Rivendell.jpg/revision/latest/scale-to-width-down/644?cb=20130202120854",
+    "Galadriel": "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Fwinteriscoming.net%2Ffiles%2F2018%2F11%2FGaladriel.jpg",
+    "Faramir": "https://static.wikia.nocookie.net/lotr/images/d/d7/Faramir_in_Two_Towers.png/revision/latest/scale-to-width-down/699?cb=20121006112209",
+    "Bilbo Baggins": "https://static.wikia.nocookie.net/lotr/images/b/b6/The_Hobbit_wallpaper_48.jpg/revision/latest/scale-to-width-down/663?cb=20131112182330",
+    "Arwen": "https://static.wikia.nocookie.net/lotr/images/d/dd/100_beautiful_arwen.jpg/revision/latest/scale-to-width-down/700?cb=20110313201140",
+    "Denethor I": "https://static.wikia.nocookie.net/lotr/images/9/98/Lord_Denethor.jpg/revision/latest/scale-to-width-down/700?cb=20150330023544",
+    "Gríma Wormtongue": "https://static.wikia.nocookie.net/lotr/images/b/b0/Grima.jpg/revision/latest/scale-to-width-down/668?cb=20080707153733",
+    "Witch-King of Angmar": "https://static.wikia.nocookie.net/lotr/images/6/6e/Witch_King.png/revision/latest/scale-to-width-down/699?cb=20120303233955",
+    "Gothmog (Lieutenant of Morgul)": "https://static.wikia.nocookie.net/lotr/images/e/e1/Lieutenant_Gothmog.jpg/revision/latest/scale-to-width-down/700?cb=20190210102106",
+    "The King of the Dead": "https://static.wikia.nocookie.net/heroes-and-villain/images/d/dd/Kingofthedead.jpg/revision/latest/top-crop/width/360/height/450?cb=20190121152038",
+    "Haldir (Haladin)": "https://static.wikia.nocookie.net/lotr/images/f/f1/Haldir.JPG/revision/latest/scale-to-width-down/398?cb=20070302122745"
+}
